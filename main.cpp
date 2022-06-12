f69bb4e3ff092eeab6e35ae05b4b673a51f23280
Thomas311926 <85378277+Thomas311926@users.noreply.github.com>
2022-06-12 13:31:30 +0800

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,31 @@
+#include <string>
+#include <iostream>
+#include <sstream>
+#include <stdio.h>
+#include <memory>
+#include <stdexcept>
+std::string exec(const char* cmd) {
+    std::array<char, 128> buffer;
+    std::string result;
+    std::unique_ptr<FILE, decltype(&pclose)> pipe(popen(cmd, "r"), pclose);
+    if (!pipe) {
+        throw std::runtime_error("popen() failed!");
+    }
+    while (fgets(buffer.data(), buffer.size(), pipe.get()) != nullptr) {
+        result += buffer.data();
+    }
+    return result;
+}
+std::string pasteit()
+{
+  return exec("pbpaste");
+}
+std::string copyit(const char * new_clipboard)
+{
+  std::stringstream cmd;
+  cmd << "echo \"" << new_clipboard << "\" | pbcopy";
+  return exec(cmd.str().c_str());
+}
+int main(){
+  
+}
